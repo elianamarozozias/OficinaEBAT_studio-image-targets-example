b0b6da99e372d473881fea8e4b9748e301f86e2f
elianzozias3D <e.a.zozias@gmail.com>
2026-08-24 00:09:36 -0300

    MELHORIAS PARA PETROBRÁS 3
    -As partículas continuam após perder target
    
    -Os conteúdos não aparecem surgindo do nada, ele surgem já grandões.

--- src/Particle Target Sync.ts
+++ src/Particle Target Sync.ts
@@ -7,16 +7,12 @@ ecs.registerComponent({
     ecs.defineState('default')
       .initial()
       .listen(world.events.globalId, 'reality.imagefound', () => {
-        // Encontrou a imagem: ativa a emissão de partículas
-        ecs.ParticleEmitter.set(world, eid, {
-          stopped: false,
-        })
+        // Encontrou a imagem: remove o Hidden para exibir as partículas
+        ecs.Hidden.remove(world, eid)
       })
       .listen(world.events.globalId, 'reality.imagelost', () => {
-        // Perdeu a imagem: pausa a emissão
-        ecs.ParticleEmitter.set(world, eid, {
-          stopped: true,
-        })
+        // Perdeu a imagem: aplica o Hidden para ocultar no mesmo frame
+        ecs.Hidden.set(world, eid)
       })
   },
 })
