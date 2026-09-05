4cb9b5c550080eb81c1d306375ea6d431d1c8627
elianzozias3D <e.a.zozias@gmail.com>
2026-09-05 17:10:28 -0300

    adicionando outras ilhas

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const onxrloaded = () => {
       require('../image-targets/toggle-slam.json'),
       require('../image-targets/waves.json'),
       require('../image-targets/metagallery_transparent.json'),
+      require('../image-targets/target_petrobras.json'),
     ],
   })
   XR8.addCameraPipelineModule(LandingPage.pipelineModule())
